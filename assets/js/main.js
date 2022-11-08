a9f72582799216fac0d9d4bdb6fe3a84c573edd5
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-08 15:42:25 +0100

    generate email

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,18 @@ const {
 createApp({
     data(){
         return{
-
+            allEmail: []
+        }
+    },
+    mounted() {
+        for (let index = 0; index < 10; index++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                console.log(response.data.response);
+                this.allEmail.push(response.data.response)
+            })
+           
         }
     }
 }).mount('#app')
