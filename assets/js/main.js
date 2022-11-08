665579609362104b7eb86f1d7bb633aa6e905454
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-08 15:10:15 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/*
+Generare 10 indirizzi email e stamparli in pagina all'interno di una lista
+ */
+
+const {
+    createApp 
+} = Vue
+
+createApp({
+    data(){
+        return{
+
+        }
+    }
+}).mount('#app')
